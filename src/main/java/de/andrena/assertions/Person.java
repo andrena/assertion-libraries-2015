98202d6f6363151209adb5a81694a496da64d7d6
Marc Philipp <mail@marcphilipp.de>
2015-10-03 13:51:10 +0200

    Remove unused Address class

--- src/main/java/de/andrena/assertions/Person.java
+++ src/main/java/de/andrena/assertions/Person.java
@@ -1,18 +1,13 @@
 package de.andrena.assertions;
 
-import static java.util.Collections.unmodifiableList;
-
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.LinkedList;
-import java.util.List;
 
 public class Person {
 
 	private final String firstName;
 	private final String lastName;
 	private final LocalDate birthday;
-	private final List<Address> addresses = new LinkedList<>();
 
 	public Person(String firstName, String lastName) {
 		this(firstName, lastName, null);
@@ -32,26 +27,8 @@ public class Person {
 		return lastName;
 	}
 
-	public void add(Address address) {
-		addresses.add(address);
-	}
-
-	public void remove(Address address) {
-		addresses.remove(address);
-	}
-
-	public List<Address> getAddresses() {
-		return unmodifiableList(addresses);
-	}
-
 	public LocalDate getBirthday() {
 		return birthday;
-	}
-
-	public Person withoutBirthday() {
-		Person person = new Person(firstName, lastName, null);
-		person.addresses.addAll(this.addresses);
-		return person;
 	}
 
 	public int getAgeInYears() {
